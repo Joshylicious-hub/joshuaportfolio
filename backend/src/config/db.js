3691f150daca37ad29b267f93bc30e9a3fc6bf75
Joshua Andres <joshuaandres098@gmail.com>
2026-07-10 21:07:13 +0800

    Enhance db.js with SSL support
    Added SSL configuration for database connection.

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,16 @@
 let mysql = require('mysql2');
+const fs = require("fs");
+const path = require("path");
 
 let db = mysql.createConnection({
     host: "gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
     user: "pvmsQbVLXrdXaws.root",
     password: "wNZPcG2S06FzHN2o",
     database: "joshua_portfolio",
-    port: 4000
+    port: 4000,
+    ssl: {
+        ca: fs.readFileSync(path.join(__dirname, "../../isrgrootx1.pem"))
+    }
 })
 
 db.connect(function(err) {
