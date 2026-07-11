c677701fc308eee664da268c8c05882c4115a237
Joshua Andres <joshuaandres098@gmail.com>
2026-07-11 10:51:06 +0800

    Configure TiDB Cloud database connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,17 +1,22 @@
+require("dotenv").config({
+    path: require("path").resolve(__dirname, "../../.env")
+});
 let mysql = require('mysql2');
 const fs = require("fs");
 const path = require("path");
 
 let db = mysql.createConnection({
-    host: "gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
-    user: "pvmsQbVLXrdXaws.root",
-    password: "wNZPcG2S06FzHN2o",
-    database: "joshua_portfolio",
-    port: 4000,
-    ssl: {
+
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+     ssl: {
         ca: fs.readFileSync(path.join(__dirname, "../../isrgrootx1.pem"))
     }
-})
+});
+
 
 db.connect(function(err) {
     if (err) throw err;
