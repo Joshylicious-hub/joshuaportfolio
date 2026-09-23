51c97d1626ab2be3f749b2915c043d0c9e2f715d
Joshua Andres <joshuaandres098@gmail.com>
2026-09-23 17:19:40 +0800

    unpublished job app

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -272,7 +272,7 @@ tech: [
     {icon: SiMysql, name: "MySQL", style: "mysql"}
 ],
 image: [joblanding, jobhome, jobai, joblanding, jobhome, jobai],
-link: "https://job-application-tracker-six-iota-21.vercel.app/"  
+link: ""  
 }, {
 id: 2,
 name: "E-commerce Web Design",
